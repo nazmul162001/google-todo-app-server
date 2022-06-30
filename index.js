5c0c8ad5f3e72c1e923580423c970e2d78a23751
nazmul162001 <nazmulprime162001@gmail.com>
2022-06-30 17:28:32 +0600

    setUp server testing running

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import route from './Route/Routes.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors())
+
+// user: googleTodo
+//paass: jFvkYNKYkIK4IaOz
+
+//mongoose connect
+mongoose.connect(process.env.MONGODB_URI,()=> {
+  console.log('mongodb connected');
+})
+
+// middleware
+app.use(bodyParser.json());
+app.use('api/user', route);
+
+app.get('/', (req,res)=> {
+  res.send('My Todos app running')
+})
+
+app.listen(port, ()=> {
+  console.log('server connected');
+})
